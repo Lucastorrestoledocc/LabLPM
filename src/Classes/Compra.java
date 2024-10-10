8a464dcc631ab59be911c59b2851322393ced50c
Santhiago Takaesu <santhiago.takaesu2021@gmail.com>
2024-10-10 11:53:31 -0300

    Autalização classe funcionário!
    Classe esatava estruturada errada, atualizei com a estrutura correta e funcinal

--- src/Classes/Compra.java
+++ src/Classes/Compra.java
@@ -1,72 +1,58 @@
 package Classes;
+
 import java.util.List;
 
 public class Compra {
-    private List<PassagemAerea> passagens;
-    private double valorTotal;
-    private double remuneracaoAgencia;
+    private List<PassagemAerea> passagens; 
+    private double valorTotal; 
+    private double remuneracaoAgencia; 
 
-    // Construtor
+    // Construtor!
     public Compra(List<PassagemAerea> passagens, double remuneracaoAgencia) {
         this.passagens = passagens;
         this.remuneracaoAgencia = remuneracaoAgencia;
-        this.valorTotal = calcularValorTotal();
+        this.valorTotal = calcularValorTotal(); 
     }
 
-    
-
+    // Getters e Setters!
     public List<PassagemAerea> getPassagens() {
         return passagens;
     }
 
-
-
     public void setPassagens(List<PassagemAerea> passagens) {
         this.passagens = passagens;
+        this.valorTotal = calcularValorTotal(); 
     }
-
-
 
     public double getValorTotal() {
         return valorTotal;
     }
 
-
-
     public void setValorTotal(double valorTotal) {
         this.valorTotal = valorTotal;
     }
-
-
 
     public double getRemuneracaoAgencia() {
         return remuneracaoAgencia;
     }
 
-
-
     public void setRemuneracaoAgencia(double remuneracaoAgencia) {
         this.remuneracaoAgencia = remuneracaoAgencia;
     }
 
-
-
-    // Método para calcular o valor total da compra
     private double calcularValorTotal() {
         double total = 0;
         for (PassagemAerea passagem : passagens) {
-            total += passagem.CalcularTarifaTotal();
+            total += passagem.CalcularTarifaTotal(); 
         }
-        return total;
+        return total; 
     }
 
-    // Método para realizar a compra
     public void RealizarCompra() {
         System.out.println("Compra realizada com sucesso! Valor total: " + valorTotal);
     }
 
-    // Método para calcular a remuneração da agência
     public double CalcularRemuneracao() {
-        return valorTotal * remuneracaoAgencia;
+        return valorTotal * remuneracaoAgencia; 
     }
 }
